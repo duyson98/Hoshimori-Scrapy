4ae2d213536ab4e6d915552ad132fcb76eefb5da
Nguyen Cong Thanh <foreverloveya321@gmail.com>
2017-06-11 16:43:25 +0800

    Fix parsing errors

--- hoshimori/hoshimori/pipelines/card_csv_pipeline.py
+++ hoshimori/hoshimori/pipelines/card_csv_pipeline.py
@@ -7,6 +7,6 @@ class CardCSVPipeline(object):
     @classmethod
     def process_item(self, item, spider):
         if item['card_type'] != 'sub':
-            item['character'] = re.split('】|（', item['card_name'])
-            item['event'] = re.split('【|】', item['card_name'])
+            item['character'] = re.split(u'\u3011|\uFF08', item['card_name'], flags=re.UNICODE)[1]
+            item['event'] = re.split(u'\u3010|\u3011', item['card_name'], flags=re.UNICODE)[1]
         return item
