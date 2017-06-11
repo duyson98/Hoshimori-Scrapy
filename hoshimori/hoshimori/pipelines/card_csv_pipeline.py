10e2f3d96db27a3a0276e51ae59c08ca4075b090
Nguyen Cong Thanh <foreverloveya321@gmail.com>
2017-06-11 17:29:26 +0800

    Fix parsing errors, return skill data in multiple lines instead of one line

--- hoshimori/hoshimori/pipelines/card_csv_pipeline.py
+++ hoshimori/hoshimori/pipelines/card_csv_pipeline.py
@@ -7,6 +7,16 @@ class CardCSVPipeline(object):
     @classmethod
     def process_item(self, item, spider):
         if item['card_type'] != 'sub':
-            item['character'] = re.split(u'\u3011|\uFF08', item['card_name'], flags=re.UNICODE)[1]
-            item['event'] = re.split(u'\u3010|\u3011', item['card_name'], flags=re.UNICODE)[1]
+            event_split = re.split(u'\u3010|\u3011', item['card_name'], flags=re.UNICODE)
+            item['character'] = re.split(u'\uFF08', event_split[2], flags=re.UNICODE)[0]
+            item['event'] = event_split[1]
+        for key in ['skill_combo', 'skill_damage', 'skill_range', 'skill_effect', 'skill_comment']:
+            # For all skill keys
+            if key.startswith('sk'):
+                # Copy skill* field
+                temp = item[key]
+                item[key] = ""
+                for substr in temp:
+                    item[key] = "%s%s\n" % (item[key], substr)
+                item[key] = item[key].rstrip("\n")
         return item
